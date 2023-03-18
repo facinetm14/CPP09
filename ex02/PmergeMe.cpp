7d23152a42b96734a0ef373ad2a4ef3c790eeff7
=Facinet Kouyate <=fakouyat@student.42wolfsburg.de>
2023-03-18 17:21:42 +0100

    benchmark done

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -19,6 +19,8 @@ void	PmergeMe::merge_me(char** args, int len)
 {
 	int i = 1, tmp, j;
 	this->_size = len;
+	float bench_vec, bench_deq;
+
 	while (args[i])
 	{
 		j = 0;
@@ -42,98 +44,13 @@ void	PmergeMe::merge_me(char** args, int len)
 		this->_deq.push_back(tmp);
 		i++;
 	}
-	this->merge_insert_sort(this->_vec);
-	this->merge_insert_sort(this->_deq);
-}
+	clock_t start = clock();
+	merge_insert_sort(this->_vec);
+	bench_vec = (float)(clock() - start);
 
-
-void	PmergeMe::merge_insert_sort(std::vector<int> & vec)
-{
-	int 							straggler;
-	std::vector<std::vector<int> >	pairs;
-	bool							is_strag =	false;
-	if (vec.size() % 2 != 0)
-	{
-		straggler = *(vec.end() - 1);
-		vec.pop_back();
-		is_strag = true;
-	}
-	pairs = this->creat_pairs(vec);
-	sort_by_larger_value(pairs);
-	vec.clear();
-	for (int i = (int)(pairs.size() - 1); i >= 0; --i)
-	{
-		vec.push_back(pairs[i][0]);
-		std::cout << pairs[i][0] << " - ";
-		std::cout << pairs[i][1] << "\n";
-	}
-	for (size_t i = 0; i < pairs.size(); ++i)
-	{
-		insert_at_right_place(vec, pairs[i][1]);
-	}
-	if (is_strag)
-		insert_at_right_place(vec, straggler);
-	std::reverse(vec.begin(), vec.end());
-	std::cout << "sorted\n";
-	for (size_t i = 0; i < vec.size(); ++i)
-	{
-		std::cout << vec[i] << "\n";
-	}
-	
-}
-
-void	PmergeMe::merge_insert_sort(std::deque<int> & deq)
-{
-	(void)deq;
-}
-
-std::vector<std::vector<int> >	PmergeMe::creat_pairs(std::vector<int> & vec)
-{
-	std::vector<std::vector<int> > all_pairs;
-	std::vector<int> pair;
-	for (size_t i = 0; i < vec.size(); i++)
-	{
-		if (i % 2 != 0)
-		{
-			if (vec[i] > vec[i - 1])
-			{
-				pair.push_back(vec[i - 1]);
-				pair.push_back(vec[i]);
-			}
-			else
-			{
-				pair.push_back(vec[i]);
-				pair.push_back(vec[i - 1]);
-			}
-			all_pairs.push_back(pair);
-			pair.clear();
-		}
-	}
-	return all_pairs;
-}
-
-void	PmergeMe::sort_by_larger_value(std::vector<std::vector<int> > & pairs)
-{
-	mergeSort(pairs, 0, pairs.size() - 1);
-}
-
-void	PmergeMe::insert_at_right_place(std::vector<int> & v, int value)
-{
-	std::vector<int>::iterator left = v.begin();
-	std::vector<int>::iterator right = v.end();
-	while (left != right)
-	{
-		if (*left < value)
-		{
-			v.insert(left, value);
-			break ;
-		}
-		if (*(right - 1) > value)
-		{
-			v.insert(right, value);
-			break ;
-		}
-		left++;
-		right--;
-	}
+	start = clock();
+	merge_insert_sort(this->_deq);
+	bench_deq = (float)(clock() - start);
+	(void)bench_deq;
+	(void)bench_vec;
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,6 +6,7 @@
 # include <vector>
 # include <sstream>
 # include <algorithm>
+# include <ctime>
 
 class	PmergeMe {
 
@@ -13,17 +14,8 @@ class	PmergeMe {
 		std::vector<int>				_vec;
 		std::deque<int> 				_deq;
 		int								_size;
-		void							merge_insert_sort(std::vector<int> & vec);
-		void							merge_insert_sort(std::deque<int> & deq);
-		std::vector<std::vector<int> >	creat_pairs(std::vector<int> & vec);
-		void							sort_by_larger_value(std::vector<std::vector<int> > & pairs);
-		void							insert_at_right_place(std::vector<int> & v, int value);
-	public:
-		void							merge_me(char**	args, int len);
-		PmergeMe(void);
-		~PmergeMe(void);
-	private:
-
+		//std::vector<std::vector<int> >	creat_pairs(std::vector<int> & vec);
+		//void							sort_by_larger_value(std::vector<std::vector<int> > & pairs);
 		template<typename T>
 		void	mergeSort(T & v, size_t l, size_t r)
 		{
@@ -75,6 +67,97 @@ class	PmergeMe {
 				i++;
 			}
 		}
+
+		template<typename T>
+		void				merge_insert_sort(T & vec)
+		{
+			int 							straggler;
+			std::vector<T>					pairs;
+			bool							is_strag =	false;
+			if (vec.size() % 2 != 0)
+			{
+				straggler = *(vec.end() - 1);
+				vec.pop_back();
+				is_strag = true;
+			}
+			pairs = this->creat_pairs(vec);
+			sort_by_larger_value(pairs);
+			vec.clear();
+			for (int i = (int)(pairs.size() - 1); i >= 0; --i)
+			{
+				vec.push_back(pairs[i][0]);
+				std::cout << pairs[i][0] << " - ";
+				std::cout << pairs[i][1] << "\n";
+			}
+			for (size_t i = 0; i < pairs.size(); ++i)
+			{
+				insert_at_right_place(vec, pairs[i][1]);
+			}
+			if (is_strag)
+				insert_at_right_place(vec, straggler);
+			std::reverse(vec.begin(), vec.end());
+			
+		}
+
+		template <typename T>
+		void				insert_at_right_place(T & v, int value)
+		{
+			typename T::iterator left = v.begin();
+			typename T::iterator right = v.end();
+			while (left != right)
+			{
+				if (*left < value)
+				{
+					v.insert(left, value);
+					break ;
+				}
+				if (*(right - 1) > value)
+				{
+					v.insert(right, value);
+					break ;
+				}
+				left++;
+				right--;
+			}
+		}
+
+		template <typename T>
+		std::vector<T>		creat_pairs(T & vec)
+		{
+			std::vector<T> all_pairs;
+			T	pair;
+			for (size_t i = 0; i < vec.size(); i++)
+			{
+				if (i % 2 != 0)
+				{
+					if (vec[i] > vec[i - 1])
+					{
+						pair.push_back(vec[i - 1]);
+						pair.push_back(vec[i]);
+					}
+					else
+					{
+						pair.push_back(vec[i]);
+						pair.push_back(vec[i - 1]);
+					}
+					all_pairs.push_back(pair);
+					pair.clear();
+				}
+			}
+			return all_pairs;
+		}
+
+		template <typename T>
+		void				sort_by_larger_value(std::vector<T> & pairs)
+		{
+			mergeSort(pairs, 0, pairs.size() - 1);
+		}
+
+	public:
+		void							merge_me(char**	args, int len);
+		PmergeMe(void);
+		~PmergeMe(void);
 };
+
 
 #endif
